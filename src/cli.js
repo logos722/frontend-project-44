8a26480855eaf2060013cf34245aeb1c73cb0782
Vladislav Kostyuk <logos535@gmail.com>
2023-01-22 21:04:56 +0300

    fix cli

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@ import readlineSync from 'readline-sync';
 const sayHi = () => {
   // eslint-disable-next-line no-console
   console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May i have your name? ');
+  const userName = readlineSync.question('May I have your name? ');
   // eslint-disable-next-line no-console
   console.log(`Hi ${userName}!`);
   return userName;
