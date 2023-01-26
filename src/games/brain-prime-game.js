8a024654baa484014993b22507ea207f75202727
Vladislav Kostyuk <logos535@gmail.com>
2023-01-26 19:05:30 +0300

    fix prime

--- src/games/brain-prime-game.js
+++ src/games/brain-prime-game.js
@@ -4,22 +4,22 @@ import { getRandomNumber } from '../utils/get-random.js';
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 function isPrime(n) {
-  if (n === 0 || n === 1) return 'no';
+  if (n === 0 || n === 1) return false;
 
   let i = 2;
   const limit = Math.sqrt(n);
   while (i <= limit) {
     if (n % i === 0) {
-      return 'no';
+      return false;
     }
     i += 1;
   }
-  return 'yes';
+  return true;
 }
 
 function primeGame() {
   const num1 = getRandomNumber();
-  const rightAnswer = isPrime(num1);
+  const rightAnswer = isPrime(num1) ? 'yes' : 'no';
   const question = `${num1}`;
   return [question, rightAnswer];
 }
